eed328a05f686b87778a166c891f986a00c53858
KoochieKun <119741750+KoochieKun@users.noreply.github.com>
2023-06-15 11:01:07 +0530

    Assignments (01-07)

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let phrase = string;
+    let reversed = '';
+    for(let i = phrase.length-1; i >= 0; i--){
+        reversed += phrase[i];
+    }
+    return reversed;
 };
 
 // Do not edit below this line
